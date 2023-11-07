07f867849068bab744a760f1d297800ea6e7ec8f
carneios-dev <sargeantmarshie@aol.com>
2023-11-07 13:43:24 -0600

    Implemented the first-fit strategy.

--- include/FirstFitStrategy.h
+++ include/FirstFitStrategy.h
@@ -1,8 +1,10 @@
 /**
  * @file FirstFitStrategy.h
- * @brief This header file contains the definition for the FirstFitStrategy class.
+ * @brief Defines the FirstFitStrategy class which implements memory allocation using the first-fit method.
  *
- * TODO: <description>
+ * This file provides the definition of the FirstFitStrategy class, which is a concrete class that inherits
+ * from the AllocationStrategy interface. The FirstFitStrategy class uses the first-fit algorithm to manage
+ * a memory pool, providing functionalities to allocate and deallocate memory blocks within that pool.
  *
  * @author Scott Swanson
  * @date November 7, 2023
@@ -13,31 +15,100 @@
 #pragma once
 
 #include "AllocationStrategy.h"
+#include <cstddef>
 
-/**
-* @brief The FirstFitStrategy class, which implements the AllocationStrategy abstract class.
-*
-* TODO: <description>
-*/
+ /**
+  * @class FirstFitStrategy
+  * @brief Implements the first-fit allocation strategy for memory management.
+  *
+  * The FirstFitStrategy class provides an implementation of a memory allocation strategy using the
+  * first-fit approach. It maintains a pool of memory and allows allocation and deallocation requests
+  * to be serviced in a way that the first free block of sufficient size is used for allocation.
+  */
 class FirstFitStrategy : public AllocationStrategy {
 public:
     /**
-     * @brief TODO: <short_description>
+     * @brief Default constructor for FirstFitStrategy.
      *
-     * TODO: <description>
+     * Initializes a new instance of the FirstFitStrategy class with default parameters.
+     */
+    FirstFitStrategy(size_t poolSize);
+
+    /**
+     * @brief Destructor for FirstFitStrategy.
+     *
+     * Cleans up any resources or pointers held by the instance.
+     */
+    virtual ~FirstFitStrategy();
+
+    /**
+     * @brief Allocates a block of memory of at least the requested size using the first-fit method.
+     *
+     * Attempts to find a free block that is large enough to fulfill an allocation request, and if
+     * successful, it carves out the required size from that block, potentially splitting it into
+     * two blocks if there's enough remaining space. If no suitable block is found, returns nullptr.
      *
      * @param size The size of the memory block to allocate.
-     * @return Returns a pointer to the start of the memory block.
+     * @return A pointer to the allocated memory block or nullptr if allocation fails.
      */
     void* Allocate(size_t size) override;
 
     /**
-     * @brief TODO: <short_description>
+     * @brief Deallocates a previously allocated block of memory.
      *
-     * TODO: <description>
+     * Marks the block of memory pointed to by the provided pointer as free and merges it with
+     * adjacent free blocks to reduce fragmentation in the memory pool.
      *
-     * @param pointer The pointer to the start of the memory block.
+     * @param pointer The pointer to the memory block to be deallocated.
      */
     void Deallocate(void* pointer) override;
-    // Implement the first-fit memory allocation logic
+
+    /**
+     * @brief Prints the current state of the memory pool for debugging purposes.
+     *
+     * Outputs information about each memory block in the pool, including its size and whether
+     * it is free or allocated. This method is useful for debugging and visualizing the state of
+     * the memory pool after a series of allocation and deallocation operations.
+     */
+    void DebugPrint();
+
+private:
+    /**
+     * @struct MemoryBlock
+     * @brief Represents a block of memory within the memory pool.
+     *
+     * This structure is used internally by the FirstFitStrategy to keep track of memory block size,
+     * allocation status, and to link all blocks in a singly linked list for memory management.
+     */
+    struct MemoryBlock {
+        size_t size;
+        bool free;
+        MemoryBlock* next;
+    };
+
+    MemoryBlock* head; /**< Pointer to the first block in the memory pool. */
+    void* memoryPool; /**< Pointer to the start of the pre-allocated memory pool. */
+
+    /**
+     * @brief Locates a free block of memory of at least the requested size.
+     *
+     * This method implements the first-fit search algorithm to find a free memory block that is
+     * large enough to satisfy a memory allocation request. If no suitable block is found, it returns
+     * nullptr indicating that the allocation cannot be satisfied with the current free blocks.
+     *
+     * @param size The minimum size of the free memory block to locate.
+     * @return A pointer to a MemoryBlock structure representing the free block, or nullptr if no
+     *         suitable block is found.
+     */
+    MemoryBlock* FindFit(size_t size);
+
+    /**
+     * @brief Coalesces adjacent free blocks into larger blocks.
+     *
+     * This method merges adjacent free memory blocks into a single larger block to prevent
+     * fragmentation within the memory pool. It should be called after a block is deallocated
+     * to ensure that the memory pool remains as contiguous as possible, which can improve the
+     * efficiency of future allocation requests.
+     */
+    void Coalesce();
 };
--- src/FirstFitStrategy.cpp
+++ src/FirstFitStrategy.cpp
@@ -1,15 +1,76 @@
 #include "pch.h"
 #include "FirstFitStrategy.h"
+#include <iostream>
 #include <stdexcept>
+#include <new> // Required for placement new.
+
+FirstFitStrategy::FirstFitStrategy(size_t poolSize) {
+    // Ensure the pool size is large enough to hold at least one MemoryBlock plus some space.
+    if (poolSize <= sizeof(MemoryBlock)) {
+        throw std::invalid_argument("Pool size must be larger than the size of MemoryBlock metadata.");
+    }
+
+    // Allocate the entire pool.
+    memoryPool = ::operator new(poolSize);
+
+    // Construct the initial MemoryBlock at the start of the pool with the correct size.
+    head = new(memoryPool) MemoryBlock{ poolSize - sizeof(MemoryBlock), true, nullptr };
+}
+
+FirstFitStrategy::~FirstFitStrategy() {
+    ::operator delete(memoryPool);
+}
 
 void* FirstFitStrategy::Allocate(size_t size) {
-    // Placeholder implementation using new
-    void* block = malloc(size);
-    if (!block) throw std::bad_alloc();
-    return block;
+    MemoryBlock* block = FindFit(size);
+    if (!block)
+        return nullptr;
+
+    if (block->size > size + sizeof(MemoryBlock)) {
+        auto* newBlock = reinterpret_cast<MemoryBlock*>(reinterpret_cast<char*>(block) + sizeof(MemoryBlock) + size);
+        newBlock->size = block->size - size - sizeof(MemoryBlock);
+        newBlock->free = true;
+        newBlock->next = block->next;
+
+        block->size = size;
+        block->next = newBlock;
+    }
+
+    block->free = false;
+
+    return reinterpret_cast<char*>(block) + sizeof(MemoryBlock);
 }
 
 void FirstFitStrategy::Deallocate(void* pointer) {
-    // Placeholder implementation using delete
-    free(pointer);
+    auto* block = reinterpret_cast<MemoryBlock*>(reinterpret_cast<char*>(pointer) - sizeof(MemoryBlock));
+    block->free = true;
+    Coalesce();
+}
+
+void FirstFitStrategy::DebugPrint() {
+    for (MemoryBlock* current = head; current != nullptr; current = current->next) {
+        std::cout << "Size: " << current->size << " bytes, Free: " << current->free << std::endl;
+    }
+}
+
+FirstFitStrategy::MemoryBlock* FirstFitStrategy::FindFit(size_t size) {
+    for (MemoryBlock* current = head; current != nullptr; current = current->next) {
+        if (current->free && current->size >= size) {
+            return current;
+        }
+    }
+
+    return nullptr;
+}
+
+void FirstFitStrategy::Coalesce() {
+    for (MemoryBlock* current = head; current && current->next; ) {
+        if (current->free && current->next->free) {
+            current->size += current->next->size + sizeof(MemoryBlock);
+            current->next = current->next->next;
+        }
+        else {
+            current = current->next;
+        }
+    }
 }
